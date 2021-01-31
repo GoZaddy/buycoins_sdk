68754fc0d94f1a012246bcf4d40646bef981a49b
GoZaddy <faruqyusuff437@gmail.com>
2021-01-31 17:56:30 -0600

    added more documentation to errors.py

--- buycoins_sdk/commons/errors.py
+++ buycoins_sdk/commons/errors.py
@@ -1,13 +1,35 @@
+"""
+This module contains errors that can be raised during the use of the SDK
+"""
+
+
 class BuycoinsException(Exception):
-    def __init__(self, error_message:str):
+    """
+    BuycoinsException is a generic exception which all represents all exceptions thrown by the Graphql API. All other
+    exceptions must inherit from this
+    """
+
+    def __init__(self, error_message: str):
+        """Create a new BuycoinsException
+
+        Args:
+            error_message: the error message thrown by the Buycoins API
+        """
         super().__init__(error_message)
 
 
 class InsufficientAmountToSellException(BuycoinsException):
-    def __init__(self, cryptocurrency: str, amount_to_sell: float):
+    """InsufficientAmountToSellException is raised when a user tries to sell more cryptocurrency than they have
+
+    """
+    def __init__(self, cryptocurrency: str, amount_to_sell: str):
+        """Create a new InsufficientAmountToSellException
+
+        Args:
+            cryptocurrency: a string representing the cryptocurrency the user tried to sell
+            amount_to_sell: the amount of coins the user tried to sell
+        """
         self.cryptocurrency = cryptocurrency
         self.amount_to_sell = amount_to_sell
         super().__init__(f"Your balance is insufficient for this sale\nCryptocurrency: {cryptocurrency}, Amount to "
                          f"sell: {amount_to_sell}")
-
-
