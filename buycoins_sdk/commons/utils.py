38af72a06d8fcbc323fe4f7f674456bde235baf7
GoZaddy <faruqyusuff437@gmail.com>
2021-02-04 00:59:32 -0600

    Added docstrings to is_valid_webhook_request

--- buycoins_sdk/commons/utils.py
+++ buycoins_sdk/commons/utils.py
@@ -3,5 +3,16 @@ This module will contain some util functions that can be used by users
 """
 
 
-def is_valid_webhook_request() -> bool:
+def is_valid_webhook_request(webhook_token: str, request_body: str, webhook_signature_header: str) -> bool:
+    """This method verifies that requests to your Webhook URL are genuine and from Buycoins.
+
+    Args:
+        webhook_token: your webhook token
+        request_body: the body of the request
+        webhook_signature_header: the X-Webhook-Signature header from BuyCoins
+
+    Returns:
+        a boolean stating whether the request is valid or not
+
+    """
     pass
