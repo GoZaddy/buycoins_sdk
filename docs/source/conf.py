242e0ce89d57bc57be5a33b08c9e7a5e3de41ba3
GoZaddy <faruqyusuff437@gmail.com>
2021-02-08 18:22:56 -0600

    doc config changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../../buycoins_sdk/'))
+sys.path.insert(0, os.path.abspath('../../buycoins_sdk/'))
 
 
 # -- Project information -----------------------------------------------------
